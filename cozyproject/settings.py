4f099a5cba575979d0b616d2fd0fab3078ca9a9a
dapoadedire <adedireadedapo19@gmail.com>
2022-05-10 16:27:00 +0100

    Password change and reset

--- cozyproject/settings.py
+++ cozyproject/settings.py
@@ -137,3 +137,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = os.environ['EMAIL_HOST']
+# EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+# EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+# EMAIL_PORT = os.environ['EMAIL_PORT']
+# EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
