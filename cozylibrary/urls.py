0e53713f35b183efb2bb9a932563c4de9241c035
dapoadedire <adedireadedapo19@gmail.com>
2022-05-10 13:42:04 +0100

    Add book detail, delete, update and add urls

--- cozylibrary/urls.py
+++ cozylibrary/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.BookListView.as_view(), name='home'),
     path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('book/new/', views.BookCreateView.as_view(), name='book_new'),
+    path('book/<int:pk>/update/', views.BookUpdateView.as_view(), name='book_update'),
+    path('book/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),
 ]
