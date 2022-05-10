b6076cfff5d62e713846688605d551c1569a624e
dapoadedire <adedireadedapo19@gmail.com>
2022-05-10 13:41:03 +0100

    Add delete, update, add views

--- cozylibrary/views.py
+++ cozylibrary/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 from . import models
 
@@ -17,3 +19,21 @@ class BookDetailView(DetailView):
     model = models.Book
     context_object_name = 'book'
     template_name = 'book_detail.html'
+
+
+class BookCreateView(CreateView):
+    model = models.Book
+    fields = '__all__'
+    template_name = 'book_new.html'
+
+
+class BookUpdateView(UpdateView):
+    model = models.Book
+    fields = '__all__'
+    template_name = 'book_update.html'
+
+
+class BookDeleteView(DeleteView):
+    model = models.Book
+    template_name = 'book_delete.html'
+    success_url = reverse_lazy('home')
